8e8a910228feb8b0eb59a63a878f65e5be0a2afc
FredTheNoob <frederikhammer02@gmail.com>
2019-09-12 14:06:15 +0200

    Implemented custom UI to form

--- ColorX/Form1.Designer.cs
+++ ColorX/Form1.Designer.cs
@@ -28,13 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnExit = new System.Windows.Forms.Label();
+            this.btnMinimize = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.borderBottom = new System.Windows.Forms.PictureBox();
+            this.borderRight = new System.Windows.Forms.PictureBox();
+            this.borderLeft = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.borderBottom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.borderRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.borderLeft)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.AutoSize = true;
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.Silver;
+            this.btnExit.Location = new System.Drawing.Point(281, -3);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(31, 24);
+            this.btnExit.TabIndex = 19;
+            this.btnExit.Text = "✖";
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            this.btnExit.MouseEnter += new System.EventHandler(this.BtnExit_MouseEnter);
+            this.btnExit.MouseLeave += new System.EventHandler(this.BtnExit_MouseLeave);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinimize.AutoSize = true;
+            this.btnMinimize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnMinimize.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinimize.ForeColor = System.Drawing.Color.Silver;
+            this.btnMinimize.Location = new System.Drawing.Point(261, -3);
+            this.btnMinimize.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(21, 24);
+            this.btnMinimize.TabIndex = 20;
+            this.btnMinimize.Text = "−";
+            this.btnMinimize.Click += new System.EventHandler(this.BtnMinimize_Click);
+            this.btnMinimize.MouseEnter += new System.EventHandler(this.BtnMinimize_MouseEnter);
+            this.btnMinimize.MouseLeave += new System.EventHandler(this.BtnMinimize_MouseLeave);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(312, 21);
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseDown);
+            // 
+            // borderBottom
+            // 
+            this.borderBottom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.borderBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.borderBottom.Location = new System.Drawing.Point(5, 359);
+            this.borderBottom.Margin = new System.Windows.Forms.Padding(2);
+            this.borderBottom.Name = "borderBottom";
+            this.borderBottom.Size = new System.Drawing.Size(302, 5);
+            this.borderBottom.TabIndex = 23;
+            this.borderBottom.TabStop = false;
+            // 
+            // borderRight
+            // 
+            this.borderRight.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.borderRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.borderRight.Location = new System.Drawing.Point(307, 21);
+            this.borderRight.Margin = new System.Windows.Forms.Padding(2);
+            this.borderRight.Name = "borderRight";
+            this.borderRight.Size = new System.Drawing.Size(5, 343);
+            this.borderRight.TabIndex = 22;
+            this.borderRight.TabStop = false;
+            // 
+            // borderLeft
+            // 
+            this.borderLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.borderLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.borderLeft.Location = new System.Drawing.Point(0, 21);
+            this.borderLeft.Margin = new System.Windows.Forms.Padding(2);
+            this.borderLeft.Name = "borderLeft";
+            this.borderLeft.Size = new System.Drawing.Size(5, 343);
+            this.borderLeft.TabIndex = 21;
+            this.borderLeft.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(312, 364);
+            this.Controls.Add(this.borderBottom);
+            this.Controls.Add(this.borderRight);
+            this.Controls.Add(this.borderLeft);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnMinimize);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.borderBottom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.borderRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.borderLeft)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label btnExit;
+        private System.Windows.Forms.Label btnMinimize;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox borderBottom;
+        private System.Windows.Forms.PictureBox borderRight;
+        private System.Windows.Forms.PictureBox borderLeft;
     }
 }
 
--- ColorX/Form1.cs
+++ ColorX/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +13,61 @@ namespace ColorX
 {
     public partial class Form1 : Form
     {
+
+        #region customUI
+
+        // Kald på et windows native så man kan flytte rundt på vinduet vha. topbaren
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // This makes the window movable
+            ReleaseCapture();
+            SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+        }
+
+        #region FormControls
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            // When the button exit is pressed the program shall close!
+            Application.Exit();
+        }
+
+        private void BtnExit_MouseLeave(object sender, EventArgs e)
+        {
+            btnExit.BackColor = Color.FromArgb(64, 64, 64);
+        }
+
+        private void BtnExit_MouseEnter(object sender, EventArgs e)
+        {
+            btnExit.BackColor = Color.Red;
+        }
+
+        private void BtnMinimize_MouseLeave(object sender, EventArgs e)
+        {
+            btnMinimize.BackColor = Color.FromArgb(64, 64, 64);
+        }
+
+        private void BtnMinimize_MouseEnter(object sender, EventArgs e)
+        {
+            btnMinimize.BackColor = Color.DimGray;
+        }
+
+        private void BtnMinimize_Click(object sender, EventArgs e)
+        {
+            // Minimize the window when the button is pressed
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        #endregion FormControls
+
+
+        #endregion customUI
         public Form1()
         {
             InitializeComponent();
